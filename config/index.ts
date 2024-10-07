5fc63b15a8955b81ee8a52be5e5e32786ff51040
pjthakur <priyanshjadon@gmail.com>
2024-10-08 02:19:57 +0530

    adding project

---
+++ config/index.ts
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+
+export const siteConfig: Metadata = {
+  title: "Linguist",
+  description:
+    "Interactive platform for language learning with lessons, quizzes, and progress tracking.",
+  keywords: [
+    "reactjs",
+    "nextjs",
+    "vercel",
+    "react",
+    "Linguist",
+    "learn-language",
+    "shadcn",
+    "shadcn-ui",
+    "radix-ui",
+    "cn",
+    "clsx",
+    "Linguist",
+    "postgresql",
+    "sonner",
+    "drizzle",
+    "zustand",
+    "mysql",
+    "lucide-react",
+    "next-themes",
+    "clerk-themes",
+    "clerk",
+    "postcss",
+    "prettier",
+    "react-dom",
+    "tailwindcss",
+    "tailwindcss-animate",
+    "ui/ux",
+    "js",
+    "javascript",
+    "typescript",
+    "eslint",
+    "html",
+    "css",
+  ] as Array<string>,
+  authors: {
+    name: "Priyansh Jadon",
+    url: "https://github.com/pjthakur",
+  },
+} as const;
+
+export const links = {
+  sourceCode: "https://github.com/pjthakur/linguist",
+} as const;
